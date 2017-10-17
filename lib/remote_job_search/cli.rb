7c870cc57d5659af75e2a7b1bbb87df993e4cf9b
Dasha Nikitina <dashadariallc@gmail.com>
2017-10-17 01:50:47 -0400

    workgin on program architecture

--- lib/remote_job_search/cli.rb
+++ lib/remote_job_search/cli.rb
@@ -2,11 +2,13 @@
 class RemoteJobSearch::CLI
 
   def call
-    puts "Enter a job category:"
     list_categories
+    menu
+    goodbye
   end
 
   def list_categories
+    puts "Enter a job category:"
     puts <<-DOC.gsub /^\s+/, ""
       1. Design
       2. Management
@@ -15,4 +17,23 @@ class RemoteJobSearch::CLI
       5. Finance
     DOC
   end
+
+  def menu
+    puts "Enter the number of the category you'd like to see jobs for or type exit:"
+    input = nil
+    while input != "exit"
+      input = gets.strip.downcase
+      case input
+        when "1"
+          puts "list jobs for category 1..."
+        when "2"
+          puts "list jobs for category 2..."
+      end
+    end
+  end
+
+  def goodbye
+    puts "See you tomorrow for more jobs"
+  end
+
 end
