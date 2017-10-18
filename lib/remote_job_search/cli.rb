eea185adb7bcdd081732c257c66c6d3c5555c13b
Dasha Nikitina <dashadariallc@gmail.com>
2017-10-18 07:09:29 -0400

    created list_categories functionality

--- lib/remote_job_search/cli.rb
+++ lib/remote_job_search/cli.rb
@@ -3,44 +3,53 @@ class RemoteJobSearch::CLI
 
   def call
     list_categories
-    list_jobs
-    menu
-    goodbye
+    # list_jobs
+    # menu
+    # goodbye
   end
 
   def list_categories
-    #expectation: scrape weworkremotely index page and list out all job categories that are available.
-    puts "Enter a job category:"
-    @jobs = RemoteJobSearch::Job.category
-    @jobs.each.with_index(1) do |job, i|
-      puts "#{i}. #{job.category}"
+    puts "Enter a job category you'd like to see remote job opportunities for:"
+    @categories = RemoteJobSearch::Scraper.categories_scraper
+    @categories.each.with_index(1) do |category, i|
+      puts "#{i}. #{category}"
     end
   end
 
-  def list_jobs
-    #expectation: scrape weworkremotely  
-    puts "Choose job title for more information"
-  end
 
-  def menu
-    input = nil
-    while input != "exit"
-      puts "Enter the number of the category you'd like to see jobs for or type list to see the categories again or type exit:"
-      input = gets.strip.downcase
-
-      if input.to_i > 0
-        job = @jobs[input.to_i-1]
-        puts "#{job.category}"
-      elsif input == "list"
-        list_categories
-      else
-        puts "Not a valid entry, type list or exit "
-      end
-    end
-  end
-
-  def goodbye
-    puts "Come back for more jobs tomorrow"
-  end
+  # def list_categories
+  #   #expectation: scrape weworkremotely index page and list out all job categories that are available.
+  #   puts "Enter a job category:"
+  #   @jobs = RemoteJobSearch::Job.category
+  #   @jobs.each.with_index(1) do |job, i|
+  #     puts "#{i}. #{job.category}"
+  #   end
+  # end
+  #
+  # def list_jobs
+  #   #expectation: scrape weworkremotely
+  #   puts "Choose job title for more information"
+  # end
+  #
+  # def menu
+  #   input = nil
+  #   while input != "exit"
+  #     puts "Enter the number of the category you'd like to see jobs for or type list to see the categories again or type exit:"
+  #     input = gets.strip.downcase
+  #
+  #     if input.to_i > 0
+  #       job = @jobs[input.to_i-1]
+  #       puts "#{job.category}"
+  #     elsif input == "list"
+  #       list_categories
+  #     else
+  #       puts "Not a valid entry, type list or exit "
+  #     end
+  #   end
+  # end
+  #
+  # def goodbye
+  #   puts "Come back for more jobs tomorrow"
+  # end
 
 end
--- lib/remote_job_search/scraper.rb
+++ lib/remote_job_search/scraper.rb
@@ -1,23 +1,40 @@
 class RemoteJobSearch::Scraper
 
-  def self.job_scraper
+
+  def self.categories_scraper
+    #Returns an array of all job categories as strings
     homepage = Nokogiri::HTML(open("https://weworkremotely.com"))
-    homepage.css("article").collect do |job_category|
-      job = {
-        :category => job_category.css("h2 a").text,
-        :status => job_category.css("ul li span.new").text,
-        :company => job_category.css("ul li a span.company").text,
-        :title => job_category.css("ul li a span.title").text,
-        :date => job_category.css("ul li a span.date").text
-        :url => "https://weworkremotely.com#{job_category.css("li a").attribute("href").value}"
-      }
-
-      job_page = Nokogiri::HTML(open(jobs[:url]))
-      binding.pry
-        job[:details] = jo_page.css("")
-        job[:apply_link] =
-
-        job
+    scraped_categories = []
+    homepage.css("h2 a").each do |job_category|
+      category_name = job_category.text.strip
+      scraped_categories << category_name
     end
+    scraped_categories.delete("")
+    scraped_categories
   end
+
+
+
+#   def self.homepage_scraper
+#     homepage = Nokogiri::HTML(open("https://weworkremotely.com"))
+#
+#     scraped_jobs_array = []
+#     homepage.css("article").each do |job_category|
+#       job = {
+#         # :category => job_category.css("h2 a").text,
+#         :company => job_category.css("ul li a span.company").text,
+#         :title => job_category.css("ul li a span.title").text,
+#         :date => job_category.css("ul li a span.date").text,
+#         :url => "https://weworkremotely.com#{job_category.css("li a").attribute("href").value}"
+#       }
+#       scraped_jobs_array << job
+#
+#       job_page = Nokogiri::HTML(open(job[:url]))
+#         job[:details] = job_page.css("div.listing-container").text
+#         job[:apply_link] = job_page.css("div.apply p a").attribute("href").value
+#
+#         scraped_jobs_array << job
+#         binding.pry
+#     end
+#     scraped_jobs_array
 end
