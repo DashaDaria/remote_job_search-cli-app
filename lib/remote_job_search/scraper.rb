490422e7ea92576890de2084b94b0b0f826b862c
Dasha Nikitina <dashadariallc@gmail.com>
2017-10-18 05:52:19 -0400

    scraping job details

--- lib/remote_job_search/scraper.rb
+++ lib/remote_job_search/scraper.rb
@@ -2,18 +2,19 @@ class RemoteJobSearch::Scraper
 
   def self.job_scraper
     homepage = Nokogiri::HTML(open("https://weworkremotely.com"))
-    homepage.css("section").collect do |job_category|
+    homepage.css("article").collect do |job_category|
       job = {
         :category => job_category.css("h2 a").text,
         :status => job_category.css("ul li span.new").text,
         :company => job_category.css("ul li a span.company").text,
         :title => job_category.css("ul li a span.title").text,
         :date => job_category.css("ul li a span.date").text
-        :url =>
+        :url => "https://weworkremotely.com#{job_category.css("li a").attribute("href").value}"
       }
 
       job_page = Nokogiri::HTML(open(jobs[:url]))
-        job[:details] =
+      binding.pry
+        job[:details] = jo_page.css("")
         job[:apply_link] =
 
         job
