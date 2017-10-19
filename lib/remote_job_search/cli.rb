fbf8676004f551f227b0b77e1d85f57e4d1e361b
Dasha Nikitina <dashadariallc@gmail.com>
2017-10-19 07:19:52 -0400

    formatted user interface

--- lib/remote_job_search/cli.rb
+++ lib/remote_job_search/cli.rb
@@ -2,24 +2,50 @@
 class RemoteJobSearch::CLI
 
   def call
-    # list_categories
+    welcome
+    list_categories
     make_design_jobs
-    list_design_jobs
-    # goodbye
+    menu
   end
-  #
-  #   def list_categories
-  #     category_names = RemoteJobSearch::Scraper.category_scraper
-  #     @category_objects = []
-  #     category_names.each do |category_name|
-  #     new_object = RemoteJobSearch::Category.new(category_name)
-  #     @category_objects << new_object
-  #   end
-  #     puts "Enter a job category you'd like to see remote job opportunities for:"
-  #     @category_objects.each.with_index(1) do |category, i|
-  #       puts "#{i}. #{category.name}"
-  #   end
-  # end
+
+    def welcome
+      puts "Welcome to your next remote job opportunity!"
+      puts "Enter a job category you'd like to see a list of jobs for:"
+      puts ""
+    end
+
+    def list_categories
+      category_names = RemoteJobSearch::Scraper.category_scraper
+      @category_objects = []
+      category_names.each do |category_name|
+      new_object = RemoteJobSearch::Category.new(category_name)
+      @category_objects << new_object
+    end
+      @category_objects.each.with_index(1) do |category, i|
+        puts "#{i}. #{category.name}"
+    end
+  end
+
+    def menu
+      input = nil
+      while input != "exit"
+        puts ""
+        puts "Type list to see the categories again or type exit."
+        input = gets.strip.downcase
+
+        if input.to_i > 0
+          list_design_jobs
+
+        elsif input == "list"
+          list_categories
+        elsif input == "exit"
+          goodbye
+          exit
+        else
+          puts "Not a valid entry, type list or exit "
+        end
+      end
+    end
 
     def make_design_jobs
       design_job_hash = RemoteJobSearch::Scraper.job_scraper
@@ -37,30 +63,7 @@ class RemoteJobSearch::CLI
       end
     end
 
-
-  # def menu
-  #   input
-
-  #
-  # def menu
-  #   input = nil
-  #   while input != "exit"
-  #     puts "Enter the number of the category you'd like to see jobs for or type list to see the categories again or type exit:"
-  #     input = gets.strip.downcase
-  #
-  #     if input.to_i > 0
-  #       job = @jobs[input.to_i-1]
-  #       puts "#{job.category}"
-  #     elsif input == "list"
-  #       list_categories
-  #     else
-  #       puts "Not a valid entry, type list or exit "
-  #     end
-  #   end
-  # end
-  #
-  # def goodbye
-  #   puts "Come back for more jobs tomorrow"
-  # end
-
+    def goodbye
+      puts "Come back tomorrow for more jobs!"
+    end
 end
