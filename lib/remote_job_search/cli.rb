71d8990e663e6db88f803b837c5f2e972debb03a
Dasha Nikitina <dashadariallc@gmail.com>
2017-10-20 21:34:18 -0400

    cleaned up code

--- lib/remote_job_search/cli.rb
+++ lib/remote_job_search/cli.rb
@@ -17,15 +17,6 @@ class RemoteJobSearch::CLI
 
   def make_jobs
     RemoteJobSearch::Scraper.job_scrape
-  #   programming = RemoteJobSearch::Category.new("Programming", RemoteJobSearch::Scraper.programming_scrape)
-  # #   design = RemoteJobSearch::Category.new("Design", RemoteJobSearch::Scraper.design_scrape)
-  #   all_other = RemoteJobSearch::Category.new("All Other", RemoteJobSearch::Scraper.all_other_scrape)
-  #   customer_support = RemoteJobSearch::Category.new("Customer Support", RemoteJobSearch::Scraper.customer_scrape)
-  #   business = RemoteJobSearch::Category.new("Business", RemoteJobSearch::Scraper.busi_scrape)
-  #   devops = RemoteJobSearch::Category.new("DevOps", RemoteJobSearch::Scraper.devops_scrape)
-  #   marketing = RemoteJobSearch::Category.new("Marketing", RemoteJobSearch::Scraper.marketing_scrape)
-  #   copywriting = RemoteJobSearch::Category.new("Copywriting", RemoteJobSearch::Scraper.copywriting_scrape)
-  end
 
   def list_categories
     RemoteJobSearch::Category.all.each.with_index(1) do |category, i|
@@ -69,21 +60,8 @@ class RemoteJobSearch::CLI
     end
   end
 
-  def goodbye
-    puts "Come back tomorrow for more jobs!"
+    def goodbye
+      puts "Come back tomorrow for more jobs!"
+    end
   end
 end
-#
-#     def list_categories
-#       category_names = RemoteJobSearch::Scraper.category_scraper
-#       @category_objects = []
-#       category_names.each do |category_name|
-#       new_object = RemoteJobSearch::Category.new(category_name)
-#       @category_objects << new_object
-#     end
-#       @category_objects.each.with_index(1) do |category, i|
-#         puts "#{i}. #{category.name}"
-#     end
-#     #Return value is an array of category objects -  #<RemoteJobSearch::Category:0x007faab51b0050 @jobs=[], @name="Design">
-#   end
-#
